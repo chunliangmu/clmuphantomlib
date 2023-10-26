80116fcf57a225312cc3917489ad3d848fafa232
Chunliang Mu <chunliang.mu@hdr.mq.edu.au>
2023-10-26 11:19:05 +1100

    Minor Updates; light.py:get_photosphere_on_ray(...) Added R1 to accpetabe calc_params

--- geometry.py
+++ geometry.py
@@ -53,7 +53,7 @@ def get_closest_pt_on_line(pt0: np.ndarray, line: np.ndarray) -> np.ndarray:
     
     Parameters
     ----------
-    pt0: N or (..., N)-dimensional array_like.
+    pt0: N or (..., N)-dimensional numpy array.
         An N-dimensional point, or M points of N dimensions.
     
     line: (2, N)-dimensional array_like, i.e. [pt1, pt2]
@@ -66,9 +66,9 @@ def get_closest_pt_on_line(pt0: np.ndarray, line: np.ndarray) -> np.ndarray:
     """
     if len(line) != 2:
         raise ValueError("Input var 'line' should have 2 points (i.e. with shape (2, N)), but ", len(line), " is not 2.")
-    pt0 = np.array(pt0, copy=False)
-    pt1 = np.array(line[0], copy=False)
-    pt2 = np.array(line[1], copy=False)
+    #pt0 = np.array(pt0, copy=False)
+    pt1 = line[0] #np.array(line[0], copy=False)
+    pt2 = line[1] #np.array(line[1], copy=False)
     t_0 = np.sum((pt0 - pt1) * (pt2 - pt1), axis=-1) / np.sum((pt2 - pt1)**2, axis=-1)
     X_t = pt1 + t_0.reshape((*t_0.shape,1)) * (pt2 - pt1)
     return X_t
