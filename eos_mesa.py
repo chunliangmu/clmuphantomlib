50757f1f0982840fdfe811ae001932b24089cb3d
Chunliang Mu <chunliang.mu@hdr.mq.edu.au>
2023-11-02 17:51:56 +1100

    removed the old mesa table read function,

--- eos_mesa.py
+++ eos_mesa.py
@@ -22,124 +22,10 @@ from .eos_base  import EoS_Base
 import os
 import numpy as np
 
-# Set global variables
 
 
 
-
-# Functions
-
-
-def _load_mesa_eos_table(settings:Settings, iverbose:int=3):
-    """Load mesa table.
-
-    Assuming mesa EoS table stored in directory settings['EoS_MESA_DATA_DIR'].
-    Required keywords in settings:
-        EoS_MESA_DATA_DIR
-        EoS_MESA_table_Z_float
-        EoS_MESA_table_Z_str
-        EoS_MESA_table_X_float
-        EoS_MESA_table_X_str
-        EoS_MESA_table_dtype
-
-    Note: data stored in MESA EoS table (var2- last column):
-    (as stated in phantom source file comments: phantom/src/main/eos_mesa_microphysics.f90 line 435-438)
-    ! The columns in the data are:
-    ! 1. logRho        2. logP          3. logPgas       4. logT
-    ! 5. dlnP/dlnrho|e 6. dlnP/dlne|rho 7. dlnT/dlnrho|e 8. dlnT/dlne|rho
-    ! 9. logS         10. dlnT/dlnP|S  11. Gamma1       12. gamma
-    
-
-    Returns
-    -------
-    (
-    mesa_table_Z_float : np.ndarray (1d),    # Z
-    mesa_table_X_float : np.ndarray (1d),    # X
-    mesa_table_logE_float : np.ndarray (1d), # Eint
-    mesa_table_logV_float : np.ndarray (1d), # V
-    ), mesa_table : np.ndarray (5d),
-        
-    """
-    # init
-    mesa_data_dir = settings['EoS_MESA_DATA_DIR']
-    mesa_table_Z_float = settings['EoS_MESA_table_Z_float']
-    mesa_table_Z_str   = settings['EoS_MESA_table_Z_str']
-    mesa_table_X_float = settings['EoS_MESA_table_X_float']
-    mesa_table_X_str   = settings['EoS_MESA_table_X_str']
-    mesa_table_dtype   = settings['EoS_MESA_table_dtype']
-    mesa_table_logE_float = None
-    mesa_table_logV_float = None
-    mesa_table_var2_str = None
-    mesa_table = None
-    no_Z = len(mesa_table_Z_float)
-    no_X = len(mesa_table_X_float)
-    
-    if mesa_data_dir is None or not os.path.isdir(mesa_data_dir):
-        raise ValueError(f"settings['MESA_DATA_DIR']={mesa_data_dir} is not a valid directory.")
-
-
-    
-    for i_Z, Z_float, Z_str in zip(range(no_Z), mesa_table_Z_float, mesa_table_Z_str):
-        # sanity check
-        if f'{Z_float:.2f}' != Z_str:
-            warn(
-                '_load_mesa_eos_table()', iverbose,
-                f"{Z_float=} is not the same as {Z_str=}.",
-            )
-        for i_X, X_float, X_str in zip(range(no_X), mesa_table_X_float, mesa_table_X_str):
-            # sanity check
-            if f'{X_float:.2f}' != X_str:
-                warn(
-                    '_load_mesa_eos_table()', iverbose,
-                    f"{X_float} is not the same as {X_str=}.",
-                )
-            with open(f"{mesa_data_dir}{os.path.sep}output_DE_z{Z_str}x{X_str}.bindata", 'rb') as f:
-
-                # load meta data
-                no_E, no_V, no_var2 = fortran_read_file_unformatted(f, 'i', 3)
-                
-                logV_float = np.array(fortran_read_file_unformatted(f, 'd', no_V))
-                if mesa_table_logV_float is None:
-                    mesa_table_logV_float = logV_float
-                elif not np.allclose(mesa_table_logV_float, logV_float):
-                    warn(
-                        '_load_mesa_eos_table()', iverbose,
-                        "Warning: logV array not the same across the data files.",
-                        "This is not supposed to happen! Check the code and the data!",
-                    )
-
-                logE_float = np.array(fortran_read_file_unformatted(f, 'd', no_E))
-                if mesa_table_logE_float is None:
-                    mesa_table_logE_float = logE_float
-                elif not np.allclose(mesa_table_logE_float, logE_float):
-                    warn(
-                        '_load_mesa_eos_table()', iverbose,
-                        "Warning: logE array not the same across the data files.",
-                        "This is not supposed to happen! Check the code and the data!",
-                    )
-
-                # init mesa_table as a structured array
-                if mesa_table is None:
-                    mesa_table = np.full((no_Z, no_X, no_E, no_V), np.nan, dtype=mesa_table_dtype)
-
-                # fill mesa table
-                for i_V in range(no_V):
-                    for i_E in range(no_E):
-                        mesa_table[i_Z, i_X, i_E, i_V] = fortran_read_file_unformatted(f, 'd', no_var2)
-                    
-            
-        
-        
-    
-    
-    #raise NotImplementedError
-    return (
-        mesa_table_Z_float,
-        mesa_table_X_float,
-        mesa_table_logE_float,
-        mesa_table_logV_float,
-        ), mesa_table
-
+# Classes
 
 
 
@@ -255,13 +141,6 @@ class EoS_MESA_table:
 
 
 
-
-
-
-
-
-
-# Classes
 
 
 class EoS_MESA(EoS_Base):
