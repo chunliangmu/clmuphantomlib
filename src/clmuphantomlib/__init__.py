1eae24e5f40e022b0d51dc7045a90f639804e4f6
Chunliang Mu <chunliang.mu@hdr.mq.edu.au>
2024-06-21 14:29:29 +1000

    Added comments to help me remember to limit code length per line.

--- src/clmuphantomlib/__init__.py
+++ src/clmuphantomlib/__init__.py
@@ -10,12 +10,14 @@ Author: Chunliang Mu (at Macquarie University, expected duration 2023-2026)
 Principal Supervisor: Professor Orsola De Marco
 
 This library is based on the wonderful python package 'sarracen'.
-It is developped as part of my PhD project "Non-adiabatic common envelope simulation of massive stars"
+It is developped as part of my PhD project
+"Non-adiabatic common envelope simulation of massive stars"
 (This project title is provisional as of 2023-10-16.)
 
 Work on this project started on 2023-01-25.
 
-Assuming temperature unit being K. Reads & handles other units from phantom data dumps.
+Assuming temperature unit being K.
+Reads & handles other units from phantom data dumps.
 
 
 Note:
@@ -25,6 +27,16 @@ MyPhantomDataFrames column names:
     h  : smoothing length
     u  : specificInternalEnergy
     T  : temperature
+
+
+-------------------------------------------------------------------------------
+
+Side note: Remember to limit line length to 79 characters according to PEP-8
+https://peps.python.org/pep-0008/#maximum-line-length
+which is the length of below line of '-' characters.
+
+-------------------------------------------------------------------------------
+
 """
 
 
